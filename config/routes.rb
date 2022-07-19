2c1039413a90f127be93ceb946e7ca4715bf798d
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-07-19 00:17:23 -0700

    commit tree 7605be9c39603c9e255d814a947b4c7a1de6fa58

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
+  #we are using all 5 resources, so don't need only
+  resources :birds 
   patch "/birds/:id/like", to: "birds#increment_likes"
 end
